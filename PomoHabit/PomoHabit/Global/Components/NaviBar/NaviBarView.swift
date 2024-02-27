8a9c981ed88c7282d91a21cac559956bf6a5c450
Yu-RiChoi <yulee0207@gmail.com>
2024-02-27 09:08:34 +0900

    [Feat] #4 - NaviBarView 구현

---
+++ PomoHabit/PomoHabit/Global/Components/NaviBar/NaviBarView.swift
@@ -0,0 +1,20 @@
+//
+//  NaviBarView.swift
+//  PomoHabit
+//
+//  Created by 최유리 on 2/27/24.
+//
+
+import UIKit
+
+class NaviBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
