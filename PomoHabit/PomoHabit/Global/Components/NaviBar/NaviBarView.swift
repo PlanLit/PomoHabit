c943fc3b5ff0617bac6937b8f7ac55ae28c384de
Yu-RiChoi <yulee0207@gmail.com>
2024-02-27 09:41:47 +0900

    [Feat] #4 - NaviBarView 구현

--- PomoHabit/PomoHabit/Global/Components/NaviBar/NaviBarView.swift
+++ PomoHabit/PomoHabit/Global/Components/NaviBar/NaviBarView.swift
@@ -7,14 +7,67 @@
 
 import UIKit
 
+import SnapKit
+
+// MARK: - NaviBarView
+
 class NaviBarView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Properties
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        
+        return label
+    }()
+    
+    private var divider: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
 
+// MARK: - Layout Helpers
+
+extension NaviBarView {
+    
+    private func setUI() {
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        addSubview(divider)
+        
+        titleLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(20)
+            $0.centerY.equalTo(self.snp.centerY)
+        }
+        
+        divider.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+            $0.width.equalTo(430)
+            $0.height.equalTo(1)
+        }
+    }
+}
+
+// MARK: - Methods
+
+extension NaviBarView {
+    func setTitle(_ title: String) {
+        titleLabel.text = title
+    }
 }
