20eaf38ed9a64f547412ff45d617862a4b3eb669
Joon Baek <joonbaek12@gmail.com>
2024-02-27 17:02:19 +0900

    [Add] #12 - TimerViewUI 관련 파일들 추가

---
+++ PomoHabit/PomoHabit/Presentation/Timer/Controllers/TimerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TimerViewController.swift
+//  PomoHabit
+//
+//  Created by Joon Baek on 2024/02/27.
+//
+
+import UIKit
+
+// MARK: - TimerViewController
+
+final class TimerViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
+
+extension TimerViewController {
+    
+}
---
+++ PomoHabit/PomoHabit/Presentation/Timer/Views/TimerView.swift
@@ -0,0 +1,34 @@
+//
+//  TimerView.swift
+//  PomoHabit
+//
+//  Created by Joon Baek on 2024/02/27.
+//
+
+import UIKit
+
+import SnapKit
+
+// MARK: - TimerView
+
+final class TimerView: UIView {
+    
+    // MARK: - UI Properties
+
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension TimerView {
+    
+}
+
