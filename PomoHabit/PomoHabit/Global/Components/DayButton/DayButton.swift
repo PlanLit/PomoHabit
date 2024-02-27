685439cda1b30c0fbe0cfdffa5b223cdb4c894c3
JiHoon K <deferare@icloud.com>
2024-02-27 09:03:54 +0900

    VStackView, HStackView 만듬
    UIKit 기반 뷰들을 담을 수 있는 컨테이너 역할

---
+++ PomoHabit/PomoHabit/Global/Components/DayButton/DayButton.swift
@@ -0,0 +1,20 @@
+//
+//  DayButton.swift
+//  PomoHabit
+//
+//  Created by JiHoon K on 2/27/24.
+//
+
+import UIKit
+
+class DayButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
