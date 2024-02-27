6c7679c0932d000953370a4e3585315eb5557c82
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-02-27 16:54:31 +0900

    [[init] #11 - 폴더 및 ViewController,View,Model 추가

---
+++ PomoHabit/PomoHabit/Presentation/WeeklyCalendar/Controllers/WeeklyCalendarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WeeklyCalendarViewController.swift
+//  PomoHabit
+//
+//  Created by 원동진 on 2/27/24.
+//
+
+import UIKit
+
+class WeeklyCalendarViewController: UIViewController {
+    private lazy var weeklyCalendarView : WeeklyCalendarView = {
+        let view = WeeklyCalendarView()
+        return view
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
