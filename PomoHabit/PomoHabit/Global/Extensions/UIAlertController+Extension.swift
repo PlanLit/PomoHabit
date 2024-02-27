4257c7cddae353f7537b26571647d4ed37b79a09
Yu-RiChoi <yulee0207@gmail.com>
2024-02-27 12:52:14 +0900

    [Feat] #6 - UIAlertController+Extension 파일 생성

--- PomoHabit/PomoHabit/Global/Extensions/UIAlertController+Extension.swift
+++ PomoHabit/PomoHabit/Global/Extensions/UIAlertController+Extension.swift
@@ -5,4 +5,21 @@
 //  Created by 최유리 on 2/27/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIAlertController {
+    func showAlert(title: String, message: String, cancelButton: Bool = true, viewController: UIViewController) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "확인", style: .default)
+        let cancel = UIAlertAction(title: "취소", style: .destructive)
+        
+        alertController.addAction(ok)
+        
+        if cancelButton {
+            alertController.addAction(cancel)
+        }
+        
+        viewController.present(alertController, animated: true)
+    }
+}
