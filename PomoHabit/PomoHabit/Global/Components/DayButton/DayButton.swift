3c74409da8918c081f65bcf96c27bf4a79c4d705
JiHoon K <deferare@icloud.com>
2024-02-27 09:25:41 +0900

    DayButton 만듬
    이거 하나하나가 월화수목금토일의 각각의 버튼이고, 버튼이 눌릴때마다 뷰 상태 바뀌는게 토글되서 DayButton 안에다가 캡슐화 한거에요.

--- PomoHabit/PomoHabit/Global/Components/DayButton/DayButton.swift
+++ PomoHabit/PomoHabit/Global/Components/DayButton/DayButton.swift
@@ -1,20 +1,84 @@
 //
 //  DayButton.swift
-//  PomoHabit
+//  myUIKitPractice
 //
-//  Created by JiHoon K on 2/27/24.
+//  Created by JiHoon K on 2/26/24.
 //
 
 import UIKit
 
+//MARK: - DayButton
+
 class DayButton: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var dayType: Day?
+    
+    /// 버튼 누르고 나서 바뀐 버튼 상태 Bool 값 반환
+    private var action: ((Bool) -> Void)?
+    
+    private var isSelected: Bool = false {
+        didSet {
+            updateButtonAppearance()
+        }
     }
-    */
+    
+    private lazy var customButton: UIButton = {
+        let button = UIButton(type: .system, primaryAction: .init(handler: { _ in
+            self.isSelected.toggle()
+            self.action?(self.isSelected)
+        }))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 49).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 49).isActive = true
+        button.layer.cornerRadius = 10
+        button.layer.borderColor = UIColor.systemGray5.cgColor
+        return button
+    }()
+    
+    init(dayType: Day, isSelected: Bool = false, action: @escaping ((Bool) -> Void)) {
+        super.init(frame: .zero)
+        self.dayType = dayType
+        self.isSelected = isSelected
+        self.action = action
+        setupButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+//MARK: - Views
+
+extension DayButton {
+    private func setupButton() {
+        addSubview(customButton)
+        NSLayoutConstraint.activate([
+            customButton.topAnchor.constraint(equalTo: topAnchor),
+            customButton.bottomAnchor.constraint(equalTo: bottomAnchor),
+            customButton.leadingAnchor.constraint(equalTo: leadingAnchor),
+            customButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        updateButtonAppearance()
+    }
+    
+    private func updateButtonAppearance() {
+        customButton.setTitle(dayType?.rawValue, for: .normal)
+        customButton.backgroundColor = isSelected ? .systemGreen : .white // 색 변경 필요
+        customButton.setTitleColor(isSelected ? .white : .black, for: .normal)
+        customButton.layer.borderWidth = isSelected ? 0 : 1
+    }
+}
+
+//MARK: - Types
+
+extension DayButton {
+    enum Day: String {
+        case mon = "월"
+        case tue = "화"
+        case wed = "수"
+        case thu = "목"
+        case fri = "금"
+        case sat = "토"
+        case sun = "일"
+    }
 }
