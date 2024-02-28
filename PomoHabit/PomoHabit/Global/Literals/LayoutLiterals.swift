86d1e11af93077df76b33f4f2b7fbbbf30ca7eef
Joon Baek <joonbaek12@gmail.com>
2024-02-28 19:27:02 +0900

    [Chore] #12 - LayoutLiterals 네이밍 수정

--- PomoHabit/PomoHabit/Global/Literals/LayoutLiterals.swift
+++ PomoHabit/PomoHabit/Global/Literals/LayoutLiterals.swift
@@ -11,9 +11,9 @@ struct LayoutLiterals {
     static let minimumVerticalSpacing = 20
     static let minimumHorizontalSpacing = 20
     
-    static let upperPrimeSpacing = 24
-    static let lowerPrimeSpacing = 24
+    static let upperPrimarySpacing = 24
+    static let lowerPrimarySpacing = 24
     
-    static let upperSubprimeSpacing = 12
-    static let lowerSubprimeSpacing = 12
+    static let upperSecondarySpacing = 12
+    static let lowerSecondarySpacing = 12
 }
