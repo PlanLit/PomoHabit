682d916e77ced8d2966ce9ca53039587bf4e735a
Joon Baek <joonbaek12@gmail.com>
2024-02-28 11:57:21 +0900

    [Add] #12 - LayoutLiterals 추가

---
+++ PomoHabit/PomoHabit/Global/Literals/LayoutLiterals.swift
@@ -0,0 +1,13 @@
+//
+//  LayoutLiterals.swift
+//  PomoHabit
+//
+//  Created by Joon Baek on 2024/02/28.
+//
+
+import Foundation
+
+struct LayoutLiterals {
+    static let minimumVerticalSpacing = 20
+    static let minimumHorizontalSpacing = 20
+}
