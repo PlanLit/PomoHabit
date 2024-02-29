a6c7bcf2f5a4fa27e66b475ea8b6398da16941d8
Joon Baek <joonbaek12@gmail.com>
2024-02-29 18:07:47 +0900

    [Add] #19 - presentBottomSheet 메서드 익스텐션화

--- PomoHabit/PomoHabit/Global/Extensions/UIViewController+Extension.swift
+++ PomoHabit/PomoHabit/Global/Extensions/UIViewController+Extension.swift
@@ -13,4 +13,17 @@ extension UIViewController {
         self.navigationController?.navigationBar.isHidden = true
     }
     
+    func presentBottomSheet(rootView: UIView, delegate: UISheetPresentationControllerDelegate? = nil, detents: [UISheetPresentationController.Detent] = [.medium(), .large()], prefersGrabberVisible: Bool = true) {
+        let viewController = BottomSheetViewController(rootView: rootView)
+        if let sheet = viewController.sheetPresentationController {
+            /// 지원할 크기 지정
+            sheet.detents = detents
+            /// delegate 설정
+            sheet.delegate = delegate
+            /// 시트 상단에 그래버 표시 (기본 값은 false)
+            sheet.prefersGrabberVisible = prefersGrabberVisible
+        }
+        
+        present(viewController, animated: true, completion: nil)
+    }
 }
