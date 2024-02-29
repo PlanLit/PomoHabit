f3f1c06318dd1622e06751de77e9ac23e6eb603f
Joon Baek <joonbaek12@gmail.com>
2024-02-29 18:06:36 +0900

    [Chore] #19 - BottomSheetVC 재사용성 개선

--- PomoHabit/PomoHabit/Global/Components/Modal/BottomSheetViewController.swift
+++ PomoHabit/PomoHabit/Global/Components/Modal/BottomSheetViewController.swift
@@ -6,20 +6,38 @@
 //
 
 import UIKit
-
 import SnapKit
 
 // MARK: - BottomSheetViewController
 
 final class BottomSheetViewController: BaseViewController {
 
-    private let containerView = WhiteNoiseView()
+    // MARK: - Properties
+    
     private var bottomConstraint: Constraint?
+    
+    // MARK: - UI Properties
+    
+    private var rootView: UIView
+    
+    // MARK: - Life Cycle
 
+    init(rootView: UIView) {
+        self.rootView = rootView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = rootView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupContainerView()
         setupGesture()
     }
 }
@@ -27,22 +45,16 @@ final class BottomSheetViewController: BaseViewController {
 // MARK: - Setups
 
 extension BottomSheetViewController {
+    private func setupContainerView() {
+        rootView.snp.makeConstraints { make in
+            make.height.equalToSuperview().multipliedBy(0.5) // 화면 높이의 절반
+            bottomConstraint = make.bottom.equalToSuperview().constraint
+        }
+    }
+    
     private func setupGesture() {
         let gesture = UIPanGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
-        containerView.addGestureRecognizer(gesture)
-    }
-}
-
-// MARK: - Layout Helpers
-
-extension BottomSheetViewController {
-    private func setupContainerView() {
-        view.addSubview(containerView)
-
-        containerView.snp.makeConstraints { make in
-            make.left.right.equalToSuperview()
-            make.bottom.equalToSuperview().offset(300)
-        }
+        rootView.addGestureRecognizer(gesture)
     }
 }
 
@@ -50,16 +62,16 @@ extension BottomSheetViewController {
 
 extension BottomSheetViewController {
     @objc private func handleGesture(_ gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: containerView)
+        let translation = gesture.translation(in: rootView)
 
         switch gesture.state {
         case .changed:
             if translation.y > 0 {
-                bottomConstraint?.update(offset: 300 + translation.y)
+                bottomConstraint?.update(inset: translation.y)
             }
         case .ended:
             UIView.animate(withDuration: 0.3) {
-                self.bottomConstraint?.update(offset: 300)
+                self.bottomConstraint?.update(inset: 0)
                 self.view.layoutIfNeeded()
             }
         default:
