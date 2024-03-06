04f941cea40fdbb6f8039275f431fb6be9842043
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-03-06 21:20:07 +0900

    [Chore] #22 -TabBarControlle item 순서, 디자인, 이름 수정

--- PomoHabit/PomoHabit/Presentation/WeeklyCalendar/Controllers/WeeklyCalendarViewController.swift
+++ PomoHabit/PomoHabit/Presentation/WeeklyCalendar/Controllers/WeeklyCalendarViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import SnapKit
+import Foundation
 
 // MARK: - WeeklyCalendarViewController
 
@@ -67,19 +68,18 @@ extension WeeklyCalendarViewController {
         weeklyCalendarView.setProgressCircleImg(offset: progressCircleOffset)
         weeklyCalendarView.setWeeklyHabbitProgressView(progress: progress)
     }
-    
 }
 
 // MARK: - Get 주간 데이터
 
 extension WeeklyCalendarViewController {
-    private func setWeeklyData(){
+    private func setWeeklyData() {
         var weeklyDates : [Int] = []
         let calendar = Calendar.current
         
         // MARK: - 현재 주의 시작 날짜
         
-        guard let result = calendar.dateInterval(of: .weekOfYear, for: Date()) else { return } // 현재 날짜가 속해있는 토요일부터 토요일까지 날짜 보여줌
+        guard let result = calendar.dateInterval(of: .weekOfYear, for: Date()) else { return } // 현재 날짜가 속해 있는 주의 첫번째/마지막 날짜
         guard let weeklyStartDate = calendar.date(byAdding: .day, value: 1, to: result.start)?.dateToString(format: "dd") else { return }
         
         // MARK: - 현재 주의 마지막 날짜
@@ -87,20 +87,20 @@ extension WeeklyCalendarViewController {
         let components = calendar.dateComponents([.year, .month], from: Date()) // 현재 날짜의 년도와 월
         guard let currentStartDate = calendar.date(from: components) else { return } // 날짜와 년도를 가지고 가장 첫번째 날짜 Get
         guard let nextStartDate = calendar.date(byAdding: .month, value: 1, to: currentStartDate) else {return} // 다음 달의 가장 첫번쨰날
-        guard let currentEndDate = calendar.date(byAdding: .day, value: -1, to: nextStartDate)?.dateToString(format: "dd") else {return} // 다음달의 가장 첫번째날 이전날 = 현재 날짜의 마지막날
+        guard let currentEndDate = calendar.date(byAdding: .day, value: -1, to: nextStartDate)?.dateToString(format: "dd") else {return} // 다음달의 가장 첫번째날 이전날 = 이번달의 마지막날
         
         // MARK: - 주간 데이터 구하기
         
-        guard let weeklyStartDateInt = Int(weeklyStartDate) else {return}
-        guard let currentEndDateInt = Int(currentEndDate) else {return}
+        guard let weeklyStartDateInt = Int(weeklyStartDate) else { return }
+        guard let currentEndDateInt = Int(currentEndDate) else { return }
         
         for i in 0...6 {
-            let date = weeklyStartDateInt+i
+            let date = weeklyStartDateInt + i
             
             if date <= currentEndDateInt {
                 weeklyDates.append(date)
-            }else{
-                weeklyDates.append(date-currentEndDateInt)
+            } else {
+                weeklyDates.append(date - currentEndDateInt)
             }
         }
         
