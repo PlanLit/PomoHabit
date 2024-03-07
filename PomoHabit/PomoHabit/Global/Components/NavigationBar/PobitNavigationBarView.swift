8393e60c9d684ce84221ad69efeae193963d8594
Joon Baek <joonbaek12@gmail.com>
2024-03-08 01:28:48 +0900

    [Chore] #27 - 타이머뷰 디자인 반영

--- PomoHabit/PomoHabit/Global/Components/NavigationBar/PobitNavigationBarView.swift
+++ PomoHabit/PomoHabit/Global/Components/NavigationBar/PobitNavigationBarView.swift
@@ -51,12 +51,11 @@ extension PobitNavigationBarView {
     
     private func setAutoLayout() {
         titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview()
+            make.centerY.equalToSuperview()
             make.leading.equalToSuperview().inset(LayoutLiterals.minimumHorizontalSpacing)
         }
         
         dividerView.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(LayoutLiterals.minimumVerticalSpacing)
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(1)
             make.bottom.equalToSuperview()
