31a14e30b13bb68dcb84e7aa1fa7e9617ef90792
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-03-20 18:23:55 +0900

    [Chore] #94 - 지훈님,준님 피드백 반영[Feat] #94 - 새로운 분기 처리적용

---
+++ PomoHabit/PomoHabit/Presentation/WeeklyCalendar/model/WeeklyHabitInfoModel.swift
@@ -0,0 +1,15 @@
+//
+//  WeeklyHabitInfoModel.swift
+//  PomoHabit
+//
+//  Created by 원동진 on 3/20/24.
+//
+
+import Foundation
+struct WeeklyHabitInfoModel {
+    var targetHabit: String?
+    var habitstate: HabitState?
+    var goalTime: Int16?
+    var duringTime: String?
+    var note: String?
+}
